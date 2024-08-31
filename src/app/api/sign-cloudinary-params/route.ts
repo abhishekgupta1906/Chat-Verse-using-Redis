26815707e53517f9a224af10a8743ddef1808492
abhishekgupta1906 <21cs3001@rgipt.ac.in>
2024-09-01 02:53:22 +0530

    error fixed

--- src/app/api/sign-cloudinary-params/route.ts
+++ src/app/api/sign-cloudinary-params/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { paramsToSign } = body;
  
-  const signature = cloudinary.utils.api_sign_request(paramsToSign,process.env.CLOUDINARY_API_SECRET);
+  const signature = cloudinary.utils.api_sign_request(paramsToSign,process.env.CLOUDINARY_API_SECRET!);
   
   return Response.json({ signature });
 }
